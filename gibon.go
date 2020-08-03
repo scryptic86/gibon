afa9ce8ab287306934c8d13eec6e6799edfb8dd2
kim (grufwub) <grufwub@gmail.com>
2020-08-03 21:36:49 +0100

    first working copy with IPFS!
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- gibon.go
+++ gibon.go
@@ -1,83 +1,177 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"path"
 	"regexp"
 	"strconv"
-	"strings"
 	"syscall"
+
+	"github.com/ipfs/go-ipfs/core"
+	"github.com/ipfs/go-ipfs/core/coreapi"
+	"github.com/ipfs/go-ipfs/core/node/libp2p"
+	"github.com/ipfs/go-ipfs/plugin/loader"
+	"github.com/ipfs/go-ipfs/repo/fsrepo"
+
+	files "github.com/ipfs/go-ipfs-files"
+	icore "github.com/ipfs/interface-go-ipfs-core"
+	icorepath "github.com/ipfs/interface-go-ipfs-core/path"
 )
 
 var (
+	// Valid paste filename check
 	validPasteRegex = regexp.MustCompile(`/([a-z]|[A-Z]|[0-9]|\.)*$`)
-	pasteMap        map[string][]byte
 )
 
-func getPaste(id string) ([]byte, bool) {
-	p, ok := pasteMap[id]
-	return p, ok
+type pasteHandler struct {
+	ctx  context.Context
+	ipfs icore.CoreAPI
 }
 
-func addPaste(id string, p []byte) {
-	pasteMap[id] = p
+func (h *pasteHandler) getPaste(cidStr string) ([]byte, error) {
+	cidPath := icorepath.New(cidStr)
+	r, err := h.ipfs.Block().Get(h.ctx, cidPath)
+	if err != nil {
+		return nil, err
+	}
+	return ioutil.ReadAll(r)
 }
 
-type pasteHandler struct{}
+func (h *pasteHandler) putPaste(paste []byte) (string, error) {
+	file := files.NewBytesFile(paste)
+	stat, err := h.ipfs.Block().Put(h.ctx, file)
+	if err != nil {
+		return "", err
+	}
+	return stat.Path().String(), nil
+}
 
 func (h *pasteHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Check for valid paste ID
-	pasteID := r.URL.EscapedPath()
-	if !validPasteRegex.MatchString(pasteID) {
+	rawPath := r.URL.EscapedPath()
+
+	// Check if we have a valid URL path
+	if !validPasteRegex.MatchString(rawPath) {
 		http.Error(w, "Illegal paste name!", http.StatusServiceUnavailable)
 		return
 	}
 
-	// Now should be in a valid paste ID format
-	pasteID = strings.TrimPrefix(pasteID, "/")
-
 	switch r.Method {
 	case "GET":
-		p, ok := getPaste(pasteID)
-		if !ok {
+		// At root send help string
+		if rawPath == "/" {
+			w.Write([]byte("Gibon -- IPFS-backed pastebin service!\n"))
+			return
+		}
+
+		// Try look for paste with CID
+		b, err := h.getPaste(rawPath)
+		if err != nil {
+			log.Println(err.Error())
 			http.Error(w, "Paste not found!", http.StatusNotFound)
 			return
 		}
-		w.Write(p)
+		w.Write(b)
 
 	case "POST":
-		_, ok := getPaste(pasteID)
-		if ok {
-			http.Error(w, "Paste with this ID already exists!", http.StatusBadRequest)
+		// If not at root, send error
+		if rawPath != "/" {
+			http.Error(w, "Please post new pastes to site root", http.StatusBadRequest)
 			return
 		}
 
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
+			log.Println(err.Error())
 			http.Error(w, "Failed reading request body", http.StatusServiceUnavailable)
 			return
 		}
 		defer r.Body.Close()
 
-		addPaste(pasteID, body)
+		cidStr, err := h.putPaste(body)
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, "Failed to put paste at CID", http.StatusInternalServerError)
+			return
+		}
+
+		w.Write([]byte(cidStr))
 	}
 }
 
+func constructIPFSNodeAPI(ctx context.Context, repoPath string) (icore.CoreAPI, error) {
+	// Load any external plugins
+	plugins, err := loader.NewPluginLoader(path.Join(repoPath, "plugins"))
+	if err != nil {
+		return nil, err
+	}
+
+	// Load preloaded and external plugins
+	err = plugins.Initialize()
+	if err != nil {
+		return nil, err
+	}
+
+	// Inject the plugins
+	err = plugins.Inject()
+	if err != nil {
+		return nil, err
+	}
+
+	// Open the repo
+	repo, err := fsrepo.Open(repoPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// Construct the node
+	node, err := core.NewNode(
+		ctx,
+		&core.BuildCfg{
+			Online:  true,
+			Routing: libp2p.DHTOption,
+			Repo:    repo,
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return core API wrapping the node
+	return coreapi.NewCoreAPI(node)
+}
+
 func main() {
-	bindAddr := flag.String("bind-addr", "localhost", "Bind to address")
-	port := flag.Uint("port", 1024, "Bind to port")
-	//cert := flag.String("cert", "", "TLS certificate file")
-	//key := flag.String("key", "", "TLS key file")
+	httpBindAddr := flag.String("http-bind-addr", "localhost", "Bind HTTP server to address")
+	httpPort := flag.Uint("http-port", 443, "Bind HTTP server to port")
+	ipfsRepo := flag.String("ipfs-repo", "/var/ipfs", "IPFS repo path")
 	flag.Parse()
 
-	pasteMap = make(map[string][]byte)
-	pasteMap["test"] = []byte("test here!\n")
+	// Get current context (cancellable)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	err := http.ListenAndServe(*bindAddr+":"+strconv.Itoa(int(*port)) /**cert, *key,*/, &pasteHandler{})
+	// Get new IPFS node API instance
+	coreAPI, err := constructIPFSNodeAPI(ctx, *ipfsRepo)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	// Create new pasteHandler instance to handle HTTP side
+	handler := &pasteHandler{
+		ctx,
+		coreAPI,
+	}
+
+	// Start HTTP server
+	httpAddr := *httpBindAddr + ":" + strconv.Itoa(int(*httpPort))
+	err = http.ListenAndServe(httpAddr /**cert, *key,*/, handler)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/grufwub/gibon
+
+go 1.14
+
+require (
+	github.com/ipfs/go-ipfs v0.6.0
+	github.com/ipfs/go-ipfs-files v0.0.8
+	github.com/ipfs/interface-go-ipfs-core v0.3.0
+)
